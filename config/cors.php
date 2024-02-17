52d0ea277c99290334696bd9c77e178b1457d247
Damith Harischandrathilaka <bqudamith@gmail.com>
2024-02-17 15:56:14 +1100

    CORD updated got MS Graph

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', '/msgraph/connect', '/msgraph/app'],
 
     'allowed_methods' => ['*'],
 
