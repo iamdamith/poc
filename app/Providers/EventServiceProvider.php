e9520cee308056cbd86d186163e30dced383811f
Damith Harischandrathilaka <bqudamith@gmail.com>
2024-02-17 23:16:34 +1100

    MS GRaph event listner updated

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -20,9 +20,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-        NewMicrosoft365SignInEvent::class => [
-            NewMicrosoft365SignInListener::class, 'handle'
-        ]
+        NewMicrosoft365SignInEvent::class,
+            [NewMicrosoft365SignInListener::class, 'handle']
+        
     ];
 
     /**
--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', '/msgraph/connect', '/msgraph/app'],
+    'paths' => ['*', 'api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['login.microsoftonline.com'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
