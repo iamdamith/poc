71eba4a48176fcd20e20ae1af687a94273b3e1bf
Damith Harischandrathilaka <bqudamith@gmail.com>
2024-02-18 01:01:57 +1100

    MSGraph CORD added

--- app/Http/Middleware/MSGraphCORS.php
+++ app/Http/Middleware/MSGraphCORS.php
@@ -17,8 +17,7 @@ class MSGraphCORS
     {
             return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods','GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+            ->header('Access-Control-Allow-Methods','GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS');
 
     }
 }
